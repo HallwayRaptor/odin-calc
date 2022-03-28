49fa18814a3862c7669b734b9d2babd89a1facaa
Rob Whelan <hallwayraptor@gmail.com>
2022-03-28 11:51:20 -0300

    add clear button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let screenValue = 0;
+
+
 //add numbers
 const add = (a,b) => a + b
 console.log(`adding 5 to 8 = ${add(5, 8)}`)
@@ -12,4 +15,43 @@ const divide = (a,b) => a / b
 console.log(`divide 40 by 8 = ${divide(40,8)}`)
 //use operator on two numbers
 const operate = (op, num1, num2) => op(num1, num2)
-console.log(`use divide function on 6 and 2 to get ${operate(divide,6,2)}`)
+console.log(`use divide function on 6 and 2 to get ${operate(divide,6,2)})`)
+
+const clearScreen = () => {
+  calcScreen.textContent = 0;
+}
+const addToScreen = () => {
+  calcScreen.textContent == 0 ? calcScreen.textContent = screenValue : calcScreen.textContent += screenValue
+}
+const addItem = (item) => screenValue = item;
+
+const calcScreen = document.getElementById('calc-screen')
+
+const buttons = Array.from(document.querySelectorAll('button'))
+console.log(buttons);
+
+const btnClear = buttons[0]
+
+btnClear.addEventListener('click', clearScreen)
+
+const btnClick = () => {
+  buttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+      switch (btn.value) {
+        case 'clear': clearScreen;
+        break;
+        case '*': ;
+        case '/': ;
+        case '-': ;
+        case '+': ;
+        case '=': ;
+        default:
+          addItem(btn.value)
+          addToScreen();
+      }
+    })
+  });
+}
+
+addToScreen();
+btnClick();
